fe1d0bd32d2482a6e165aefe2087bd763465626f
hurter3 <mark.hurter@hotmail.co.uk>
2020-02-05 13:40:21 +0000

    rm env.py from cache and put in .gitignore

--- env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@cluster0-p0o9u.mongodb.net/movie-review-hub?retryWrites=true&w=majority"
-os.environ["MONGO_DBNAME"] = "movie-review-hub"
