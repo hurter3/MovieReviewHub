d1bd1c2fe764db73a4df0f063744a9338e158537
hurter3 <mark.hurter@hotmail.co.uk>
2020-02-05 13:35:39 +0000

    add routes with dummy extend html files for base

--- app.py
+++ app.py
@@ -1,15 +1,47 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME') 
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI') 
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template("categories.html", 
+                           categories=mongo.db.categories.find())
+
+@app.route("/search")
+def search():
+    return render_template('search.html', title='Search')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html', title='Sign Up')
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=5000,
             debug=True)
