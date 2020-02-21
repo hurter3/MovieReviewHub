a7ed6093d446b051860c5fc2a8e101ab0c44ca89
hurter3 <mark.hurter@hotmail.co.uk>
2020-02-21 11:18:32 +0000

    rearranged the onreadystatechange and open as the httprequest is not working

--- static/js/main.js
+++ static/js/main.js
@@ -2,20 +2,26 @@ function searchMovie() {
     let xhr = new XMLHttpRequest();  
     let query = document.getElementById("movieForm").movie_name;
     
-
-    xhr.open("GET", "//api.themoviedb.org/3/search/movie?api_key=" + config[0] + "&language=en-US&query=" + query + "&page=1&include_adult=false");
-    xhr.send();
-    
+   
     xhr.onreadystatechange = function() {
+        document.getElementById("data").innerHTML = this.responseText;
+        debugger;
         if (this.readyState == 4 && this.status == 200) {
+            displayText(this.responseText);
             console.log(this.responseText);
         }
         else {
-            console.log("no data");  
+            console.log("no data");
+            debugger;  
             console.log(this.readyState);
-            console.log(this.status);          
+            debugger;
+            console.log(this.status);
+            debugger;          
         }
     };
+    
+    xhr.open("GET", "//api.themoviedb.org/3/search/movie?api_key=" + config[0] + "&language=en-US&query=" + query + "&page=1&include_adult=false");
+    xhr.send();
 }
 
 function displayText(data) {
