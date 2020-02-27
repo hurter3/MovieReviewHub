f1cf9915643f01a7c2160d60967525654f6119f6
hurter3 <mark.hurter@hotmail.co.uk>
2020-02-27 09:55:43 +0000

    rearranged the element order of the listings to be more UX

--- static/js/main.js
+++ static/js/main.js
@@ -17,20 +17,17 @@ function searchMovie() {
     for (let i in data.results) {
         if (data.results[i].poster_path === null) {
         } else {
-        let poster = "https://image.tmdb.org/t/p/w200" + data.results[i].poster_path; 
-        let img= "<img class='media-poster card-header' src=" + poster + " alt='image'></img>";
+        let movie_header = "<div><h4>" + data.results[i].title + "</h4></div>";
+        let poster = "https://image.tmdb.org/t/p/w200" + data.results[i].poster_path;
+        let movie_overview = data.results[i].overview;
         let movie_title = "'" + data.results[i].title + "'";
-        let urlfor = `<a href="{{ url_for('insertmovie', title= ${movie_title} )}}">`;
-        //let urlfor = `<a href=\"{{ url_for(\"insertmovie\",title=`+ movie_title + `) }}\"`;
-        //let urlfor = '<a href=\"{{ url_for(\"insertmovie\",title=' + movie_title + ') }}\"';
-
-        list += '<li>' + img + urlfor + data.results[i].title + '</a></li>' +
-        '<span>' + data.results[i].overview + '</span>';
+        let sel_movie_btn = `<a href="{{ url_for('insertmovie', title= ${movie_title} )}}"type="button" class="btn btn-success btn-sm">Reviews</a>`;
+        let img= "<div><img class='media-poster card-header' src=" + poster + " alt='image'></img>"+ sel_movie_btn + "</div>";
+        list += '<li>' + movie_header + img + '</a></li>' + '<span>' + movie_overview + '</span>';
         }
-//    <img class="media-poster card-header" src="{{movie.url}}" alt="1917"></img> 
-//  <a href="{{ url_for('reviews', movie_id=movie._id) }}"      
 }
-    
+
+
     document.getElementById("data").innerHTML = "<ul>" + list + "</ul>";
 
     $("li").click(function() {
