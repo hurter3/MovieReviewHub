f3483972e32e1f1d2206305f0dd50dc36c0ee1b8
hurter3 <mark.hurter@hotmail.co.uk>
2020-03-05 12:12:05 +0000

    added more js functionality as the data.results is different for movies than person

--- static/js/main-latest.js
+++ static/js/main-latest.js
@@ -1,11 +1,12 @@
 // onclick="search(); return false;" add this to search.html
-function searchMovie() {
-    let film = document.getElementById("movie_name").value;
+// https://api.themoviedb.org/3/search/person?api_key=51977f79d5e5c6689cf9b9ec7dcc8716&language=en-US&query=roger%20moore&page=1&include_adult=false
+
+function searchMovie(searchBy) {
+    let query = document.getElementById("searchInput").value;
     let key = config.apiKey;
-    // alert(film + key);
     $.ajax({    
         type: 'GET',
-        url : "https://api.themoviedb.org/3/search/movie?api_key=" + key + "&language=en-US&query=" + film + "&page=1&include_adult=false",
+        url : "https://api.themoviedb.org/3/search/" + seachBy + "?api_key=" + key + "&language=en-US&query=" + query + "&page=1&include_adult=false",
         async: false,
         data: {
             format: 'json'
@@ -17,31 +18,34 @@ function searchMovie() {
     for (let i in data.results) {
         if (data.results[i].poster_path === null) {
         } else {
-
-        let title=data.results[i].title;    
-        let select_movie_btn =  "<button onclick='selectMovie(" + title + ")'class='btn btn-success'>Select Movie</button>";
-
-        let movie_title = "<div><h4 id='movie_title'>" + data.results[i].title + "</h4></div>";
-        let poster = "https://image.tmdb.org/t/p/w200" + data.results[i].poster_path; 
-        let img= "<div><img class='media-poster card-header' src=" + poster + " alt='image'></img>"+ select_movie_btn + "</div>";
-        //let urlfor = '<a href=\"{{ url_for(\"insertmovie\",title=' + movie_title + ') }}\"';
-        
-        list += '<li>' + movie_title + img  + '<span>' + data.results[i].overview + '</span>';
+        let movie_header = "<div><h4 id='movie_title'>" + data.results[i].title + "</h4></div>";
+        let poster = "https://image.tmdb.org/t/p/w200" + data.results[i].poster_path;
+        let tmdb_id = data.results[i].id;
+        let movie_overview = data.results[i].overview;
+        let movie_title = data.results[i].title;
+        let release_date = data.results[i].release_date;
+        let vote_average = data.results[i].vote_average;
+        let sel_movie_btn = `<button onclick="selectMovie(\'${tmdb_id}\',\'${movie_title}\',\'${poster}\',\'${release_date}\',\'${vote_average}\',\'${movie_overview}\')" class="btn btn-success">Select movie to review</button>`;
+        let img= "<img class='media-poster card-header' src=" + poster + " alt='image'></img>";
+        list += '<div class="row item-border"><div class="col-md-4">'+ img + '</div><div class="col-md-8"><div>' + movie_header + '</div><p>'+ movie_overview + '</p>'+sel_movie_btn+'</div></div></div>' ;
         }
-//    <img class="media-poster card-header" src="{{movie.url}}" alt="1917"></img> 
-//  <a href="{{ url_for('reviews', movie_id=movie._id) }}"""      
+  
 }
-    
     document.getElementById("data").innerHTML = "<ul>" + list + "</ul>";
-
-    $("li").click(function() {
-        alert(this.textContent);
-    });
     }     
     });
 }
 
-function selectMovie(title) {
-    console.log("select movie has been invoked");
-    console.log(document.getElementById("movie_title").innerHTML);
+
+
+function selectMovie(tmdb_id,movie_title,poster,release_date,vote_average,movie_overview) {
+    console.log('selectmovie');
+    debugger;
+    document.getElementById('form_tmdb_id').value = tmdb_id; 
+    document.getElementById('form_movie_title').value = movie_title; 
+    document.getElementById('form_poster_url').value = poster; 
+    document.getElementById('form_movie_overview').value = movie_overview; 
+    document.getElementById('form_release_date').value = release_date;  
+    document.getElementById('form_vote_average').value = vote_average; 
+   document.getElementById('formInsertMovie').submit();
 }
--- static/js/main.js
+++ static/js/main.js
@@ -4,22 +4,33 @@
 function searchMovie(searchBy) {
     let query = document.getElementById("searchInput").value;
     let key = config.apiKey;
+    console.log(searchBy);
+    console.log(query);
+    console.log(key);
+    debugger;
+    
     $.ajax({    
         type: 'GET',
-        url : "https://api.themoviedb.org/3/search/" + seachBy + "?api_key=" + key + "&language=en-US&query=" + query + "&page=1&include_adult=false",
+        url : "https://api.themoviedb.org/3/search/" + searchBy + "?api_key=" + key + "&language=en-US&query=" + query + "&page=1&include_adult=false",
         async: false,
         data: {
             format: 'json'
         },
         success: function(data) {
-    console.log(data);
     let list = "";
-    
-    for (let i in data.results) {
+    console.log(data);
+    debugger;
+
+    if (searchBy==="movie"){
+        for (let i in data.results) {
+        console.log(data.results[i].poster_path);
+        debugger;
         if (data.results[i].poster_path === null) {
         } else {
         let movie_header = "<div><h4 id='movie_title'>" + data.results[i].title + "</h4></div>";
         let poster = "https://image.tmdb.org/t/p/w200" + data.results[i].poster_path;
+        console.log(poster);
+        debugger;
         let tmdb_id = data.results[i].id;
         let movie_overview = data.results[i].overview;
         let movie_title = data.results[i].title;
@@ -31,6 +42,26 @@ function searchMovie(searchBy) {
         }
   
 }
+    } else {
+        for (let i in data.results) {
+
+            for (j in data.results[i].known_for) {
+                let movie_header = "<div><h4 id='movie_title'>" + data.results[i].known_for[j].title + "</h4></div>";
+                let poster = "https://image.tmdb.org/t/p/w200" + data.results[i].poster_path;
+                console.log(poster);
+                debugger;
+                let tmdb_id = data.results[i].known_for[j].id;
+                let movie_overview = data.results[i].known_for[j].overview;
+                let movie_title = data.results[i].known_for[j].title;
+                let release_date = data.results[i].known_for[j].release_date;
+                let vote_average = data.results[i].known_for[j].vote_average;
+                let sel_movie_btn = `<button onclick="selectMovie(\'${tmdb_id}\',\'${movie_title}\',\'${poster}\',\'${release_date}\',\'${vote_average}\',\'${movie_overview}\')" class="btn btn-success">Select movie to review</button>`;
+                let img= "<img class='media-poster card-header' src=" + poster + " alt='image'></img>";
+                list += '<div class="row item-border"><div class="col-md-4">'+ img + '</div><div class="col-md-8"><div>' + movie_header + '</div><p>'+ movie_overview + '</p>'+sel_movie_btn+'</div></div></div>' ;
+        ;}
+            }
+    }
+
     document.getElementById("data").innerHTML = "<ul>" + list + "</ul>";
     }     
     });
