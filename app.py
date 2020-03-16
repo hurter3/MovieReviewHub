c712638425e8ecfdcbb3bee5592809ee2c21f125
hurter3 <mark.hurter@hotmail.co.uk>
2020-03-16 15:45:12 +0000

    removed wtf forms

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
-from forms import RegistrationForm, LoginForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from datetime import datetime
